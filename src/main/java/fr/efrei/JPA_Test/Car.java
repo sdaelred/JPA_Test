55f82dc5d399af2820e9f071c9b4aa53f623760a
Aelred <sdaelred@gmail.com>
2019-11-27 17:14:04 +0100

    TP3 -> TP 1
    Ajout des fichiers du TP 3 dans le TP 1

--- src/main/java/fr/efrei/JPA_Test/Car.java
+++ src/main/java/fr/efrei/JPA_Test/Car.java
@@ -1,46 +1,107 @@
 package fr.efrei.JPA_Test;
 
-import java.util.ArrayList;//Certains imports sont innutilisés, nous les laissons pour
+import java.util.ArrayList;//Certains imports sont inutilisés, nous les laissons pour
 import java.util.List;     // quand nous pourrons les utiliser, 
                            //lorsque nous aurons la correction des TPs.
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import java.util.Date;
+
+import javax.persistence.*;
 
 @Entity
-public class Car {//Par cohésion avec les autres TP, nous n'avons gardé que la classe "Car".
-public Car() {
-		super();
-		// TODO Auto-generated constructor stub
+public class Car {
+	
+	@Id
+	private long id;
+	private String plateNumber;
+	private String brand;
+	private int price;
+	private boolean isRented;
+	private int numberOfSeat;
+	private String begin;
+	
+	public long getId() {
+		return id;
 	}
 
-@Id
-@GeneratedValue(strategy = GenerationType.AUTO)
-private Long id;
-private int plateNumber;
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getBegin() {
+		return begin;
+	}
 
+	public void setBegin(String begin) {
+		this.begin = begin;
+	}
 
-public long getId() {
-	return id;
-}
-public void setId(long id) {
-	this.id = id;
-}
-public int getPlateNumber() {
-	return plateNumber;
-}
+	public String getEnd() {
+		return end;
+	}
 
-public void setPlateNumber(int plateNumber) {
-	this.plateNumber = plateNumber;
-}
-@Override
-public String toString() {
-	return "Car [plateNumber=" + plateNumber + "]";
-}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+	private String end;
+	
+	public int getNumberOfSeat() {
+		return numberOfSeat;
+	}
+
+	public void setNumberOfSeat(int numberOfSeat) {
+		this.numberOfSeat = numberOfSeat;
+	}
+
+	public boolean isRented() {
+		return isRented;
+	}
+
+	public void setRented(boolean isRented) {
+		this.isRented = isRented;
+	}
+
+	public Car() {
+		super();
+	}
+	
+	public Car(String plateNumber, String brand, int numberOfSeat, int price, boolean isRented) {
+		super();
+		this.plateNumber = plateNumber;
+		this.brand = brand;
+		this.price = price;
+		this.numberOfSeat = numberOfSeat;
+		this.isRented = isRented;
+	}
+	
+	public String getPlateNumber() {
+		return plateNumber;
+	}
+	
+	public void setPlateNumber(String plateNumber) {
+		this.plateNumber = plateNumber;
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [plateNumber=" + plateNumber + ", brand=" + brand + ", numberOfSeat=" + numberOfSeat + ", price=" + price + "]";
+	}
 
 }
--- src/main/java/fr/efrei/JPA_Test/CarRestWebService.java
+++ src/main/java/fr/efrei/JPA_Test/CarRestWebService.java
@@ -1,21 +1,76 @@
 package fr.efrei.JPA_Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class CarRestWebService {
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 
-	CarRepository carRepository;
+@RestController
+public class CarRestWebService{
 	
-	@Autowired
-	public CarRestWebService(CarRepository carRepository) {
-		super();
-		this.carRepository = carRepository;
+	private List<Car> cars = new ArrayList<Car>();
+	
+	public CarRestWebService() {//Constructeur avec deux voitures de bases
+		cars.add(new Car("11AA22", "Ferrari", 2, 1000, true));
+		cars.add(new Car("33BB44", "Porshe", 2, 2222, false));
 	}
 	
-	@GetMapping("/cars")
-	public Iterable<Car> getCars(){
-		return carRepository.findAll();
+	
+	
+	@RequestMapping(value="/cars", method=RequestMethod.GET) 
+	@ResponseStatus(HttpStatus.OK) 
+	public List<Car> getListOfCars(){//Renvoie simplement la liste des voitures de la classe
+		return cars;
 	}
+	
+	@RequestMapping(value = "/cars", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void addCar(@RequestBody Car car) throws Exception{//Ajoute une voiture dans la liste
+		System.out.println(car);
+		cars.add(car);
+	}
+	
+	@RequestMapping(value = "/cars/{plateNumber}", method = RequestMethod.GET) 
+	@ResponseStatus(HttpStatus.OK) 
+	@ResponseBody
+	public Car aCar(@PathVariable("plateNumber") String plateNumber) throws Exception{ 
+		//Cette fonctione renvoie la première voiture (supposée la seule...) avec la même
+		//plaque d'immatriculation que celle donnée en paramètre : @PathVariable("plateNumber") String plateNumber
+		for(Car car : cars) {
+			if(car.getPlateNumber().equals(plateNumber)) {
+				return car;
+			}
+		}
+		return null;
+	}
+	
+	
+	@RequestMapping(value = "/cars/{plateNumber}", method = RequestMethod.DELETE) 
+	@ResponseStatus(HttpStatus.OK) 
+	public void getBack(@PathVariable("plateNumber") String plateNumber) throws Exception{
+		Car theCar = aCar(plateNumber);
+		theCar.setRented(false);
+		theCar.setBegin(null);
+		theCar.setEnd(null);
+	}
+	
+	@RequestMapping(value = "/cars/{plateNumber}", method = RequestMethod.PUT) 
+	@ResponseStatus(HttpStatus.OK) 
+	public void rent(@PathVariable("plateNumber") String plateNumber, @RequestBody(required = false) String dates) throws Exception{
+		Car theCar = aCar(plateNumber);
+		aCar(plateNumber).setRented(true);
+		theCar.setBegin(dates);
+		theCar.setEnd("Deux mois après le " + dates);//Location de deux mois
+	}//Ne pouvant pas tester notre code, nous avons "simulé" une location de deux mois de base.
+	//Dans la vraie vie, les dates entrées seraient de vraies dates proposées par l'utilisateur.
 }
